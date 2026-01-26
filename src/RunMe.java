279f4389adf1c12cdc4772a6c5c0765ca0b6f063
Sid Senthil <sidsenthil2010@gmail.com>
2026-01-26 12:13:46 -0800

    add flatlaf

--- src/RunMe.java
+++ src/RunMe.java
@@ -1,9 +1,13 @@
 import com.jogamp.newt.Display;
 import core.DisplayWindow;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+
 public class RunMe {
     public static void main(String[] args) {
         // --== Load an image to filter ==--
+
+        //FlatDarkLaf.setup();
         DisplayWindow.showFor("images/8.jpg", 1280, 720, "DoNothingFilter");
 
         // --== Determine your input interactively with menus ==--
