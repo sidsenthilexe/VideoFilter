7be48f36f9354f70ee58106bd3a67448c01d2883
Sid Senthil <sidsenthil2010@gmail.com>
2026-01-28 09:12:22 -0800

    add first step (masking for white)

--- src/cardRecognition/CardFilter.java
+++ src/cardRecognition/CardFilter.java
@@ -1,14 +1,52 @@
 package cardRecognition;
 
 import Interfaces.PixelFilter;
+import cardRecognitionUtil.Constants;
 import core.DImage;
 
 public class CardFilter implements PixelFilter {
 
+    // NOTE: Test image resize is 1000x750
+
     @Override
     public DImage processImage(DImage img) {
+        short[][] red = img.getRedChannel();
+        short[][] green = img.getGreenChannel();
+        short[][] blue = img.getBlueChannel();
 
-        return null;
+        short[][] filteredR = new short[red.length][red[0].length];
+        short[][] filteredG = new short[green.length][green[0].length];
+        short[][] filteredB = new short[blue.length][blue[0].length];
+
+        filterColors(red, green, blue, filteredR, filteredG, filteredB, Constants.ColorThresholds.CARD);
+
+        img.setColorChannels(filteredR, filteredG, filteredB);
+        return img;
+    }
+
+    public void filterColors(short[][] red, short[][] green, short[][] blue,
+                            short[][] fRed, short[][] fGreen, short[][] fBlue, Constants.ColorThresholds color) {
+
+        for (int r = 0; r < red.length; r++) {
+            for (int c = 0; c < red[r].length; c++) {
+                double redDist = Math.abs(red[r][c] - color.R());
+                double greenDist = Math.abs(green[r][c] - color.G());
+                double blueDist = Math.abs(blue[r][c] - color.B());
+
+                double overallDist = Math.sqrt((redDist * redDist) + (greenDist * greenDist) + (blueDist * blueDist));
+
+                if (overallDist < color.DIST()) {
+                    fRed[r][c] = 255;
+                    fGreen[r][c] = 255;
+                    fBlue[r][c] = 255;
+                } else {
+                    fRed[r][c] = 0;
+                    fGreen[r][c] = 0;
+                    fBlue[r][c] = 0;
+                }
+            }
+        }
+
     }
 
 }
---
+++ src/cardRecognitionUtil/Constants.java
@@ -0,0 +1,25 @@
+package cardRecognitionUtil;
+
+public final class Constants {
+
+    public enum ColorThresholds {
+        CARD((short) 255, (short) 255, (short) 255, 160);
+
+        private final short R, G, B;
+        private final int DIST;
+
+        ColorThresholds(short R, short G, short B, int DIST) {
+            this.R = R;
+            this.G = G;
+            this.B = B;
+            this.DIST = DIST;
+        }
+
+        public short R() { return R; }
+        public short G() { return G; }
+        public short B() { return B; }
+        public int DIST() { return DIST; }
+
+    }
+
+}
