a1103e85c53807a8ee5493e8f04e2352b5ed9205
Sid Senthil <sidsenthil2010@gmail.com>
2026-02-04 09:34:42 -0800

    new method of finding card corners Check for close by preexisting corners also.

--- src/cardRecognition/CardFilter.java
+++ src/cardRecognition/CardFilter.java
@@ -13,6 +13,11 @@ public class CardFilter implements PixelFilter, Drawable {
 
     // NOTE: Test image resize is 1000x750
     ArrayList<Card> cards = new ArrayList<>();
+    ArrayList<Integer> topLeftX = new ArrayList<>();
+    ArrayList<Integer> topLeftY = new ArrayList<>();
+    ArrayList<Integer> bottomRightX = new ArrayList<>();
+    ArrayList<Integer> bottomRightY = new ArrayList<>();
+
 
     @Override
     public DImage processImage(DImage img) {
@@ -20,25 +25,131 @@ public class CardFilter implements PixelFilter, Drawable {
         short[][] green = img.getGreenChannel();
         short[][] blue = img.getBlueChannel();
 
+
         short[][] filteredR = new short[red.length][red[0].length];
         short[][] filteredG = new short[green.length][green[0].length];
         short[][] filteredB = new short[blue.length][blue[0].length];
 
 
-        filterColors(red, green, blue, filteredR, filteredG, filteredB, Colors.CARD);
-        cards = findCardCornersNew(filteredR, filteredG, filteredB);
+        short[][] bwGrid = filterColors(red, green, blue, filteredR, filteredG, filteredB, Colors.CARD);
+
+        findCorners(bwGrid);
 
         img.setColorChannels(filteredR, filteredG, filteredB);
         return img;
     }
 
-    @Override
-    public void drawOverlay(PApplet window, DImage original, DImage filtered) {
-        drawCardCorners(window, cards);
+    private void findCorners(short[][] grid) {
+
+        topLeftX.clear();
+        topLeftY.clear();
+        bottomRightX.clear();
+        bottomRightY.clear();
+
+        int searchSize = 25;
+
+        for (int r = searchSize; r + searchSize < grid.length; r++) {
+            for (int c = searchSize; c + searchSize < grid[0].length; c++) {
+
+                boolean surroundingBlack = true;
+
+                for (int i = 1; i <= searchSize; i++) {
+
+                    if (grid[r-i][c] != 0) surroundingBlack = false;
+                    if (grid[r][c-i] != 0) surroundingBlack = false;
+                    if (grid[r-i][c-i] != 0) surroundingBlack = false;
+                }
+
+                boolean surroundingWhite = true;
+
+                for (int i = 1; i <= searchSize; i++) {
+                    if (grid[r+i][c] != 255) surroundingWhite = false;
+                    if (grid[r][c+i] != 255) surroundingWhite = false;
+                    if (grid[r+i][c+i] != 255) surroundingWhite = false;
+                }
+
+                boolean checkNear = true;
+                double maxDist = 50d;
+                for (int i = 0; i < topLeftX.size(); i++) {
+                    int x = (topLeftX.get(i) - r);
+                    int y = (topLeftY.get(i) - c);
+                    double distance = Math.sqrt(x * x + y * y);
+                    if (distance <= maxDist) {
+                        checkNear = false;
+                        break;
+                    }
+                }
+
+                if (grid[r][c] == 255 && surroundingBlack && surroundingWhite && checkNear) {
+                    topLeftX.add(r);
+                    topLeftY.add(c);
+                }
+            }
+        }
+
+        for (int r = searchSize; r + searchSize< grid.length; r++) {
+            for (int c = searchSize; c + searchSize < grid[0].length; c++) {
+
+                boolean surroundingBlack = true;
+
+                for (int i = 1; i <= searchSize; i++) {
+
+
+                    if (grid[r+i][c] != 0) surroundingBlack = false;
+                    if (grid[r][c+i] != 0) surroundingBlack = false;
+                    if (grid[r+i][c+i] != 0) surroundingBlack = false;
+                }
+
+                boolean surroundingWhite = true;
+
+                for (int i = 1; i <= searchSize; i++) {
+                    if (grid[r-i][c] != 255) surroundingWhite = false;
+                    if (grid[r][c-i] != 255) surroundingWhite = false;
+                    if (grid[r-i][c-i] != 255) surroundingWhite = false;
+                }
+
+                boolean checkNear = true;
+                double maxDist = 50d;
+                for (int i = 0; i < bottomRightX.size(); i++) {
+                    int x = (bottomRightX.get(i) - r);
+                    int y = (bottomRightY.get(i) - c);
+                    double distance = Math.sqrt(x * x + y * y);
+                    if (distance <= maxDist) {
+                        checkNear = false;
+                        break;
+                    }
+                }
+
+                if (grid[r][c] == 255 && surroundingBlack && surroundingWhite && checkNear) {
+                    bottomRightX.add(r);
+                    bottomRightY.add(c);
+                }
+            }
+        }
+
+
     }
 
-    private void filterColors(short[][] red, short[][] green, short[][] blue,
+    @Override
+    public void drawOverlay(PApplet window, DImage original, DImage filtered) {
+        for (int i = 0; i < topLeftX.size(); i++) {
+            window.stroke(255,0,0);
+            window.fill(255,0,0);
+            window.ellipse(topLeftY.get(i), topLeftX.get(i), 5,5);
+            System.out.println(topLeftX.size());
+        }
+
+        for (int i = 0; i < bottomRightX.size(); i++) {
+            window.stroke(255, 0, 0);
+            window.fill(255, 0, 0);
+            window.ellipse(bottomRightY.get(i), bottomRightX.get(i), 5, 5);
+        }
+    }
+
+    private short[][] filterColors(short[][] red, short[][] green, short[][] blue,
                             short[][] fRed, short[][] fGreen, short[][] fBlue, Colors color) {
+
+        short[][] bwGrid = new short[red.length][red[0].length];
 
         for (int r = 0; r < red.length; r++) {
             for (int c = 0; c < red[r].length; c++) {
@@ -52,190 +163,20 @@ public class CardFilter implements PixelFilter, Drawable {
                     fRed[r][c] = 255;
                     fGreen[r][c] = 255;
                     fBlue[r][c] = 255;
+                    bwGrid[r][c] = 255;
                 } else {
                     fRed[r][c] = 0;
                     fGreen[r][c] = 0;
                     fBlue[r][c] = 0;
+                    bwGrid[r][c] = 0;
                 }
             }
         }
 
-    }
-
-    private ArrayList<Card> findCardCornersNew(short[][] red, short[][] green, short[][] blue) {
-        int scanHeight = 10;
-        int scanWidth = 10;
-
-        for (int r = 0; r + scanHeight < red.length; r += scanHeight) {
-            for (int c = 0; c + scanWidth < red[0].length; c += scanWidth) {
-
-                boolean[][] currentScan = new boolean[scanHeight][scanWidth];
-
-                for (int r2 = 0; r2 < scanHeight; r2++) {
-                    for (int c2 = 0; c2 < scanWidth; c2++) {
-
-                        currentScan[r2][c2] = (red[r][c] == Colors.CARD.R() &&
-                                green[r][c] == Colors.CARD.G() &&
-                                blue[r][c] == Colors.CARD.B());
-
-                    }
-                }
-
-                parseScanBox(currentScan, r, c);
-
-
-            }
-        }
-
-        return cards;
-    }
-
-    private void parseScanBox(boolean[][] scan, int x, int y) {
-       boolean topLeft = false, topRight = false, bottomLeft = false, bottomRight = false;
-
-       double numWhite = 0, totalPixels = 0;
-
-        for (int r = 0; r < scan.length/2; r++) {
-            for (int c = 0; c < scan[0].length/2; c++) {
-                totalPixels++;
-                if (scan[r][c]) numWhite++;
-            }
-        }
-
-        if (numWhite >= totalPixels * 0.8) topLeft = true;
-        numWhite = 0;
-        totalPixels = 0;
-
-        for (int r = 0; r < scan.length/2; r++) {
-            for (int c = scan[0].length/2; c < scan[0].length; c++) {
-                totalPixels++;
-                if (scan[r][c]) numWhite++;
-            }
-        }
-
-        if (numWhite >= totalPixels * 0.8) topRight = true;
-        numWhite = 0;
-        totalPixels = 0;
-
-        for (int r = scan.length/2; r < scan.length; r++) {
-            for (int c = 0; c < scan[0].length/2; c++) {
-                totalPixels++;
-                if (scan[r][c]) numWhite++;
-            }
-        }
-
-        if (numWhite >= totalPixels * 0.8) bottomLeft = true;
-        numWhite = 0;
-        totalPixels = 0;
-
-        for (int r = scan.length/2; r < scan.length; r++) {
-            for (int c = scan[0].length/2; c < scan[0].length; c++) {
-                totalPixels++;
-                if (scan[r][c]) numWhite++;
-            }
-        }
-
-        if (numWhite >= totalPixels * 0.8) bottomRight = true;
-
-        if (topRight && !topLeft && !bottomLeft && !bottomRight) {
-            cards.add(new Card(x, y, x, y));
-        }
-    }
-
-
-
-
-    private ArrayList<Card> findCardCorners(short[][] red, short[][] green, short[][] blue) {
-
-        boolean[] whiteCols = new boolean[red[0].length];
-        boolean[] whiteRows = new boolean[red.length];
-
-        for (int c = 0; c < red[0].length; c++) {
-            int numWhite = 0;
-
-            for (int r = 0; r < red.length; r++) {
-
-                if (red[r][c] == Colors.CARD.R() &&
-                    green[r][c] == Colors.CARD.G() &&
-                    blue[r][c] == Colors.CARD.B()) {
-                    numWhite ++;
-                }
-            }
-
-            whiteCols[c] = numWhite > red[0].length / 3;
-        }
-
-        for (int r = 0; r < red.length; r++) {
-            int numWhite = 0;
-            for (int c = 0; c < red[0].length; c++) {
-
-                if (red[r][c] == Colors.CARD.R() &&
-                        green[r][c] == Colors.CARD.G() &&
-                        blue[r][c] == Colors.CARD.B()) {
-                    numWhite ++;
-                }
-            }
-
-            whiteRows[r] = numWhite > red.length / 3;
-        }
-
-        int countTrueCols = 0;
-        for (boolean c : whiteCols) {
-            if (c) countTrueCols++;
-        }
-
-        int countTrueRows = 0;
-        for (boolean r : whiteRows) {
-            if (r) countTrueRows++;
-        }
-
-        System.out.println("True Cols: " + countTrueCols);
-        System.out.println("True Rows: " + countTrueRows);
-
-        ArrayList<Card> cards = getCards(whiteCols, whiteRows);
-
-        System.out.println("Cards: " + cards.size());
-
-        return cards;
-    }
-
-    private static ArrayList<Card> getCards(boolean[] whiteCols, boolean[] whiteRows) {
-        ArrayList<Integer> cardStartsCol = new ArrayList<>();
-        ArrayList<Integer> cardStartsRow = new ArrayList<>();
-        ArrayList<Integer> cardEndsCol = new ArrayList<>();
-        ArrayList<Integer> cardEndsRow = new ArrayList<>();
-
-        for (int col = 1; col < whiteCols.length; col++) {
-            if (whiteCols[col] && !whiteCols[col - 1]) { cardStartsCol.add(col); }
-            if (!whiteCols[col] && whiteCols[col-1]) { cardEndsCol.add(col-1); }
-        }
-
-        for (int row = 1; row < whiteRows.length; row++) {
-            if (whiteRows[row] && !whiteRows[row-1]) { cardStartsRow.add(row); }
-            if (!whiteRows[row] && whiteRows[row-1]) { cardEndsRow.add(row-1); }
-        }
-
-        ArrayList<Card> cards = new ArrayList<>();
-
-        for (int i = 0; i < cardStartsRow.size(); i++) {
-            for (int j = 0; j < cardStartsCol.size(); j++) {
-                cards.add(new Card(cardStartsRow.get(i), cardStartsCol.get(j), cardEndsRow.get(i), cardEndsCol.get(i)));
-            }
-        }
-        return cards;
-    }
-
-    private void drawCardCorners(PApplet window, ArrayList<Card> cards) {
-        window.fill( window.color(255, 0, 0) );
-        window.stroke( window.color(255, 0, 0) );
-        if (!cards.isEmpty()) {
-            for (Card c : cards) {
-                window.ellipse(c.getX1(), c.getY1(), 5, 5);
-                window.ellipse(c.getX2(), c.getY2(), 5, 5);
-            }
-        }
+        return bwGrid;
 
     }
+
 
 
 }
